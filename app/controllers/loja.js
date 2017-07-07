85190be576b8ebe3c043add431bbd98cd486d821
Junior Silva <juniorpsilva7@gmail.com>
2017-07-07 16:57:38 -0300

    page 186

--- app/controllers/loja.js
+++ app/controllers/loja.js
@@ -1,60 +1,29 @@
 // app/controllers/loja.js
 
-var lojas = [
-    {_id: 1, nome:'Loja Exemplo 1',
-     email: 'loja1@loja1.com.br'},
-     {_id: 2, nome:'Loja Exemplo 2',
-     email: 'loja2@loja2.com.br'},
-     {_id: 3, nome:'Loja Exemplo 3',
-     email: 'loja3@loja3.com.br'},
-];
-
-module.exports = function(){
-    var ID_LOJA_INC = 3;
+module.exports = function(app){
+    
+    var Loja = app.models.loja;
+    
     var controller = {};
 
     controller.listaLojas = function(req, res){
-        res.json(lojas);
+        Loja.find().exec()
+            .then(
+                function(lojas){
+                    res.json(lojas);
+                },
+                function(erro){
+                    console.error(erro);
+                    res.status(500).json(erro);
+                }
+            );   
     };
 
-    controller.obtemLoja = function(req, res){
-        var idLoja = req.params.id;
-        var loja = lojas.filter(function(loja){
-            return loja._id == idLoja;
-        })[0];
-        loja ? res.json(loja) : res.status(404).send('Loja não encontrada');
-    };
+    controller.obtemLoja = function(req, res){};
 
-    controller.removeLoja = function(req, res){
-        var idLoja = req.params.id;
-        lojas = lojas.filter(function(loja){
-            return loja._id != idLoja;
-        });
-        res.status(204).end();
-    };
+    controller.removeLoja = function(req, res){};
 
-    controller.salvaLoja = function(req, res){
-
-        var loja = req.body;
-        loja = loja._id ? atualiza(loja) : adiciona(loja);
-        res.json(loja);
-    };
-
-    function adiciona(lojaNova){
-        lojaNova._id = ++ID_LOJA_INC;
-        lojas.push(lojaNova);
-        return lojaNova;
-    }
-
-    function atualiza(lojaAlterar){
-        lojas = lojas.map(function(loja){
-            if(loja._id == lojaAlterar._id){ 
-                loja = lojaAlterar; 
-            }
-            return loja;
-        });
-        return lojaAlterar;
-    }
+    controller.salvaLoja = function(req, res){};
 
     return controller;
 };
---
+++ app/models/loja.js
@@ -0,0 +1,21 @@
+// app/models/loja.js
+
+var mongoose = require('mongoose');
+
+module.exports = function(){
+    var schema = mongoose.Schema({
+        nome: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            index: {
+                unique: true  //chave primaria nao pode ter duas lojas com mesmo email
+            }            
+        }
+    });
+
+    return mongoose.model('Loja', schema);
+};
