0d2e2fc6845e23ad646cf851c7f731ec385dc773
Junior Silva <juniorpsilva7@gmail.com>
2017-10-12 16:55:13 -0300

    Delete Loja cascade Produtos - WORKING

---
+++ dbStart.bat
@@ -0,0 +1,3 @@
+cd c:\Mongo\bin
+mongod --dbpath c:\node\meanapp1\data\
+pause
--- public/js/controllers/LojasController.js
+++ public/js/controllers/LojasController.js
@@ -1,15 +1,15 @@
 // public/js/Controllers/LojasController.js
 
 angular.module('meanapp1').controller('LojasController',
-    function ($scope, Loja, Produto, $http) {
+    function ($scope, Loja, Produto, $http, $q) {
 
         $scope.lojas = [];
-        $scope.produtosLojaDelete = []; //var que contera a lista de produtos da loja que sera deletada
+        var produtosLojaDelete = []; //var que contera a lista de produtos da loja que sera deletada
 
         $scope.filtro = '';
 
         $scope.mensagem = { texto: '' };
-        
+
         function buscaLojas() {
             Loja.query(function (lojas) {
                 $scope.lojas = lojas;
@@ -26,59 +26,97 @@ angular.module('meanapp1').controller('LojasController',
 
         $scope.remove = function (loja) {
             if (confirm('Tem certeza que deseja remover essa Loja?')) {
-                if(lojaHasProdValidation(loja._id)){ // se tiver produtos apagar cascade
-                    if (confirm('Essa Loja possui produtos cadastrados\nDeseja remover também todos os produtos cadastrados?')) {
+                $q.all([
+                    queryProdutos()
+                ]).then(function (data) {
+                    var produtosResource = data[0];
+                    var lojaHasProd;
+                    angular.forEach(produtosResource, function (prod, index) {
+                        if (prod.prodLoja._id == loja._id) {
+                            produtosLojaDelete[produtosLojaDelete.length] = prod._id;
+                        }
+                    });
 
-                        // Loja.delete({ id: loja._id },
-                        //     buscaLojas, // callback de sucesso - recarrega a lista na view dpois da remoção
-                        //     function (erro) {   // callback de falha
-                        //         console.log(erro);
-                        //         $scope.mensagem = { texto: "Não foi posível remover a loja" };
-                        //     }
-                        // );
-                        console.log('faz de conta que apagou 1');
-
+                    if (produtosLojaDelete.length > 0) {
+                        lojaHasProd = true; // se tem produtos
+                    } else {
+                        lojaHasProd = false; // se nao tem produtos
                     }
-                } else { //Se nao tiver produtos deleta normal
-                    
-                    // Loja.delete({ id: loja._id },
-                    //     buscaLojas, // callback de sucesso - recarrega a lista na view dpois da remoção
-                    //     function (erro) {   // callback de falha
-                    //         console.log(erro);
-                    //         $scope.mensagem = { texto: "Não foi posível remover a loja" };
-                    //     }
-                    // );
-                    console.log('faz de conta que apagou 2');
 
-                }
+                    if (lojaHasProd) { // se a loja tiver produtos cadastrados
+                        if (confirm('Essa Loja possui produtos cadastrados\nDeseja remover também todos os produtos cadastrados?')) {
+                            // apagar cada produto dessa loja
+                            angular.forEach(produtosLojaDelete, function (prod, index) {
+                                console.log('Deletar produto: ' + produtosLojaDelete[index]);
+                                Produto.delete({ id: produtosLojaDelete[index] },
+                                    buscaLojas, // callback de sucesso - recarrega a lista na view dpois da remoção
+                                    function (erro) {   // callback de falha
+                                        console.log(erro);
+                                        $scope.mensagem = { texto: "Não foi posível remover o produto" };
+                                    }
+                                );
+                            });
+
+                            Loja.delete({ id: loja._id },
+                                buscaLojas, // callback de sucesso - recarrega a lista na view dpois da remoção
+                                function (erro) {   // callback de falha
+                                    console.log(erro);
+                                    $scope.mensagem = { texto: "Não foi posível remover a loja" };
+                                }
+                            );
+                            console.log('apagou 1 - LOJA');
+                            produtosLojaDelete.length = 0;
+                        }
+                    } else { // se a loja nao tiver produtos
+                        Loja.delete({ id: loja._id },
+                            buscaLojas, // callback de sucesso - recarrega a lista na view dpois da remoção
+                            function (erro) {   // callback de falha
+                                console.log(erro);
+                                $scope.mensagem = { texto: "Não foi posível remover a loja" };
+                            }
+                        );
+                        console.log('faz de conta que apagou 2 - LOJA');
+                        produtosLojaDelete.length = 0;
+                    }
+                });
+                // var lojaHasProd = lojaHasProdValidation(loja._id);
+                // if(lojaHasProd){ // se tiver produtos apagar cascade
+                //     if (confirm('Essa Loja possui produtos cadastrados\nDeseja remover também todos os produtos cadastrados?')) {
+
+                //         // Loja.delete({ id: loja._id },
+                //         //     buscaLojas, // callback de sucesso - recarrega a lista na view dpois da remoção
+                //         //     function (erro) {   // callback de falha
+                //         //         console.log(erro);
+                //         //         $scope.mensagem = { texto: "Não foi posível remover a loja" };
+                //         //     }
+                //         // );
+                //         console.log('faz de conta que apagou 1');
+
+                //     }
+                // } else { //Se nao tiver produtos deleta normal
+
+                //     // Loja.delete({ id: loja._id },
+                //     //     buscaLojas, // callback de sucesso - recarrega a lista na view dpois da remoção
+                //     //     function (erro) {   // callback de falha
+                //     //         console.log(erro);
+                //     //         $scope.mensagem = { texto: "Não foi posível remover a loja" };
+                //     //     }
+                //     // );
+                //     console.log('faz de conta que apagou 2');
+
+                // }
 
             } else {
                 //nada
             }
         };
 
-        //HELP FUNCTION
-        function lojaHasProdValidation(idLoja){
-            console.log(idLoja);
-            Produto.query(function (produtos) {
-                angular.forEach(produtos, function (prod, index) {
-                    if(prod.prodLoja._id == idLoja ){
-                        console.log(prod.prodLoja._id + ' ' + index);
-                        $scope.produtosLojaDelete[index] = prod._id;
-                    }
-                });
-
-                if($scope.produtosLojaDelete.length > 0){
-                    console.log('true');
-                    console.log($scope.produtosLojaDelete);
-                    return true;
-                } else{
-                    console.log('false');
-                    console.log($scope.produtosLojaDelete);
-                    return false;
-                }
+        function queryProdutos() {
+            var d = $q.defer();
+            var result = Produto.query(function () {
+                d.resolve(result);
             });
-            
+            return d.promise;
         }
 
     });
